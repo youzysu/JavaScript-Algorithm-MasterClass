fba54e4d262859d9778b705f5561be41183153c4
youzysu <111998760+youzysu@users.noreply.github.com>
2022-10-26 12:17:32 +0900

    :heavy_check_mark: singly-linked-list-push

---
+++ Data structure/Singly_Linked_List_practice.js
@@ -0,0 +1,30 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+
+    //push method
+    push(value) {
+        let newNode = new Node(value);
+        if(this.head === null) {
+            this.head = newNode
+            this.tail = this.head
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++;
+        return this;
+    }
+
+    //push method
+}
