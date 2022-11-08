44619b12c6b391fdd4d7e0c9382789dc29d7a08d
youzysu <111998760+youzysu@users.noreply.github.com>
2022-11-08 19:35:25 +0900

    feat: [SLL] remove method

--- Data structure/Singly_Linked_List_practice.js
+++ Data structure/Singly_Linked_List_practice.js
@@ -1,113 +1,126 @@
 class Node {
-    constructor(value) {
-        this.value = value
-        this.next = null
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class SinglyLinkedList {
-    constructor() {
-        this.head = null
-        this.tail = null
-        this.length = 0
-    }
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
 
-    push(value) {
-        let newNode = new Node(value);
-        if(this.head === null) {
-            this.head = newNode
-            this.tail = this.head
-        } else {
-            this.tail.next = newNode
-            this.tail = newNode
-        }
-        this.length++;
-        return this;
+  push(value) {
+    let newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
+    this.length++;
+    return this;
+  }
 
-    pop() {
-        if(!this.head) return undefined
-        let cur = this.head
-        let pre = cur
-        while (cur.next) {
-            pre = cur
-            cur = cur.next
-        }
-        this.tail = pre
-        pre.next = null
-        this.length--
-        if(this.length === 0) {
-            this.head = null
-            this.tail = null
-        }
-        return cur
+  pop() {
+    if (!this.head) return undefined;
+    let cur = this.head;
+    let pre = cur;
+    while (cur.next) {
+      pre = cur;
+      cur = cur.next;
     }
-
-    shift() {
-        if(!this.head) return undefined
-        let shifted = this.head
-        this.head = this.head.next
-        this.length--;
-        if(this.length === 0) {
-            this.tail = null;
-        }
-        return shifted
+    this.tail = pre;
+    pre.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
     }
+    return cur;
+  }
 
-    unshift(val) {
-        const newNode = new Node(val)
-        if (!this.head) {
-            this.head = newNode
-            this.tail = newNode
-        } else {
-            newNode.next = this.head
-            this.head = newNode
-        }
-        this.length++
-        return this
+  shift() {
+    if (!this.head) return undefined;
+    let shifted = this.head;
+    this.head = this.head.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
     }
+    return shifted;
+  }
 
-    get(index) {
-        if (index < 0 || index >= this.length) return null
-        let current = this.head
-        let count = 0
-        while (count !== index) {
-            current = current.next
-            count++
-        }
-        return current
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
     }
-    
-    set(index, value) {
-        const updatedNode = this.get(index)
-        if (updatedNode) {
-        updatedNode.value = value
-        return true
-        }
-        return false
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let current = this.head;
+    let count = 0;
+    while (count !== index) {
+      current = current.next;
+      count++;
     }
+    return current;
+  }
 
-    insert(index, value) {
-        if (index < 0 || index > this.length) return false
-        if (index === this.length) return !!this.push(value)
-        if (index === 0) return !!this.unshift(value)
-
-
-        const newNode = new Node(value)
-        const prev = this.get(index - 1)
-        const cur = prev.next
-        prev.next = newNode
-        newNode.next = cur
-        this.length++
-        return true
+  set(index, value) {
+    const updatedNode = this.get(index);
+    if (updatedNode) {
+      updatedNode.value = value;
+      return true;
     }
+    return false;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return this.push(value);
+    if (index === 0) return !!this.unshift(value);
+
+    const newNode = new Node(value);
+    const prev = this.get(index - 1);
+    const cur = prev.next;
+    prev.next = newNode;
+    newNode.next = cur;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index > this.length) return undefined;
+    if (index === this.length) return this.pop();
+    if (index === 0) return this.shift();
+
+    const prev = this.get(index - 1);
+    const cur = this.get(index);
+
+    prev.next = cur.next;
+    this.length--;
+
+    return cur.value;
+  }
 }
 
-
 // test
-const list = new SinglyLinkedList()
-list.push("0")
-list.push("1")
-list.push("2") // 왜 길이가 두번 더해지지?
-list.insert(3, "3")
-console.log(list)
+const list = new SinglyLinkedList();
+list.push("zero");
+list.push("one");
+list.push("two");
+console.log(list);
+console.log(list.remove(2));
+console.log(list);
