0a32a9df9a98c8fa622e58b92cd736922825dca2
youzysu <111998760+youzysu@users.noreply.github.com>
2022-11-11 09:34:04 +0900

    feat: BST insert method

--- Data structure/BST_practice.js
+++ Data structure/BST_practice.js
@@ -10,5 +10,32 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
-  insert(val) {}
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      }
+      if (value > current.value) {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
 }
