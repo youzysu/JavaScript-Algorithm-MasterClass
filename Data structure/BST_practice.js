5e04ffb88735180dd70aefd1de5d4d2b8be77ab9
youzysu <111998760+youzysu@users.noreply.github.com>
2022-11-12 13:38:55 +0900

    feat: BST find, contain method

--- Data structure/BST_practice.js
+++ Data structure/BST_practice.js
@@ -38,4 +38,54 @@ class BinarySearchTree {
       }
     }
   }
+
+  find(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    let found = false;
+
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      }
+      if (value > current.value) {
+        current = current.right;
+      }
+      if (value === current.value) {
+        found = true;
+      }
+    }
+
+    if (!found) return undefined;
+    return current;
+  }
+
+  contain(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+
+    while (current) {
+      if (value < current.value) {
+        current = current.left;
+      }
+      if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
+
+const tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
+
+console.log(tree.contain(17));
