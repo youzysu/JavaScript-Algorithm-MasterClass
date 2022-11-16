afc2fd5890220fff046b618cd996c5a91207c3ce
youzysu <111998760+youzysu@users.noreply.github.com>
2022-11-16 10:48:34 +0900

    feat: Binary Search Tree DFS method

--- Data structure/DFS_practice.js
+++ Data structure/DFS_practice.js
@@ -94,4 +94,51 @@ class BinarySearchTree {
 
     return nodeValues;
   }
+
+  DFS_PreOrder() {
+    const nodeValues = [];
+    const traverse = (node) => {
+      nodeValues.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    };
+
+    traverse(this.root);
+    return nodeValues;
+  }
+
+  DFS_PostOrder() {
+    const nodeValues = [];
+    const traverse = (node) => {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      nodeValues.push(node.value);
+    };
+
+    traverse(this.root);
+    return nodeValues;
+  }
+
+  DFS_InOrder() {
+    const nodeValues = [];
+    const traverse = (node) => {
+      if (node.left) traverse(node.left);
+      nodeValues.push(node.value);
+      if (node.right) traverse(node.right);
+    };
+
+    traverse(this.root);
+    return nodeValues;
+  }
 }
+
+const tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+console.log(tree.DFS_PreOrder()); // [ 10, 6, 3, 8, 15, 20 ]
+console.log(tree.DFS_PostOrder()); // [ 3, 8, 6, 20, 15, 10 ]
+console.log(tree.DFS_InOrder()); // [ 3, 6, 8, 10, 15, 20 ]
