77391a0b89a75692ab0c74c4bbbbf460ea876fa2
youzysu <111998760+youzysu@users.noreply.github.com>
2022-11-18 09:26:42 +0900

    feat: Max Binary Heap extracTop method

--- Data structure/Max_Binary_Heap_practice.js
+++ Data structure/Max_Binary_Heap_practice.js
@@ -1,6 +1,7 @@
 class MaxBinaryHeap {
   constructor() {
     this.values = [];
+    // [Point] index = n인 value의 child의 index는 2n + 1, 2n + 2이다.
   }
 
   insert(element) {
@@ -24,9 +25,47 @@ class MaxBinaryHeap {
       }
     }
   }
-}
 
-// [Point] index = n인 value의 child의 index는 2n + 1, 2n + 2에 위치한다.
+  extractTop() {
+    const root = this.values[0];
+    const end = this.values.pop();
+
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+
+    return root;
+  }
+
+  sinkDown() {
+    let index = 0;
+    const shifted = this.values[0];
+
+    while (true) {
+      let leftIndex = 2 * index + 1;
+      let rightIndex = leftIndex + 1;
+      let left = this.values[leftIndex];
+      let right = this.values[rightIndex];
+      let swapIndex = null;
+
+      if (left && left > shifted) {
+        swapIndex = leftIndex;
+      }
+      if (
+        (right && swapIndex === null && right > shifted) ||
+        (right && swapIndex !== null && right > left)
+      ) {
+        swapIndex = rightIndex;
+      }
+      if (swapIndex === null) break;
+
+      this.values[index] = this.values[swapIndex];
+      this.values[swapIndex] = shifted;
+      index = swapIndex;
+    }
+  }
+}
 
 let heap = new MaxBinaryHeap();
 heap.insert(41);
@@ -38,4 +77,24 @@ heap.insert(12);
 heap.insert(55);
 console.log(heap);
 heap.insert(38);
+console.log(heap);
+heap.extractTop();
+console.log(heap);
+heap.extractTop();
+console.log(heap);
+heap.extractTop();
+console.log(heap);
+heap.extractTop();
+console.log(heap);
+heap.extractTop();
+console.log(heap);
+heap.extractTop();
+console.log(heap);
+heap.extractTop();
+console.log(heap);
+heap.extractTop();
+console.log(heap);
+heap.extractTop();
+console.log(heap);
+heap.insert(999);
 console.log(heap);
