45d1a610140166136679514075f57530d0ff48fc
youzysu <111998760+youzysu@users.noreply.github.com>
2022-11-24 09:40:05 +0900

    feat: Hash Table set, get method

---
+++ Data structure/Hash_Table_Practice.js
@@ -0,0 +1,57 @@
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  #hash(key) {
+    let total = 0;
+    const PRIME_NUMBER = 31;
+
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      const value = key.charCodeAt(i) - 96;
+      total = (total * PRIME_NUMBER + value) % this.keyMap.length;
+    }
+
+    return total;
+  }
+
+  // 왜 key, value를 array에 저장할까?
+  set(key, value) {
+    const index = this.#hash(key);
+    if (!this.keyMap[index]) this.keyMap[index] = [];
+    this.keyMap[index].push([key, value]);
+  }
+
+  get(key) {
+    const index = this.#hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) return this.keyMap[index][i][1];
+      }
+    }
+    return undefined;
+  }
+
+  // keyMap도 array 대신 map을 사용해서 구현할 수 있나?
+  set2(key, value) {
+    const index = this.#hash(key);
+    if (!this.keyMap[index]) this.keyMap[index] = new Map();
+    this.keyMap[index].set(key, value);
+  }
+
+  get2(key) {
+    const index = this.#hash(key);
+    if (this.keyMap[index]) {
+      return this.keyMap[index].has(key)
+        ? this.keyMap[index].get(key)
+        : undefined;
+    }
+  }
+}
+
+const ht = new HashTable(5);
+ht.set('maroon', '#800000');
+ht.set('rmaoon', '#FFFF00');
+console.log(ht.get('rmaoon'));
+console.log(ht.get('rmaooa'));
+console.log(ht);
